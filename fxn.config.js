df0ea8d4da2c13b808ac970b2b0090620d98cfc3
rahikaiser <rahikaiser@live.com>
2023-11-25 15:15:31 +0600

    nov25b

--- fxn.config.js
+++ fxn.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  apps: [
-    {
-      name: "fxnew",
-      script: "npm",
-      args: "run start -- -p 3004",
+  apps : [{
+    name: 'fxnew',
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start -p 3004', //running on port 3000
+    cwd: "./apps/nextapp",
+    instances: 1,
+    watch: false,
+    env: {
+        ...
     },
-  ],
+  }]
 };
